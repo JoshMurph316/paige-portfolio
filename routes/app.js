18893d33c53b2c1920dbedf22adc08ccd403ccac
Joshmurph316 <Joshmurph316@gmail.com>
2017-07-04 21:39:44 -0400

    started work on gallery

--- routes/app.js
+++ routes/app.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+router.get('/message/:msg', function(req, res, next) {
+  res.render('node', {message: req.params.msg});
 });
 
+router.post('/message',function(req, res, next){
+  var message = req.body.message;
+  res.redirect('/message' + message);
+})
 module.exports = router;
