1d2b65c17627549c14624cafc1c58bbce778f231
Joshmurph316 <Joshmurph316@gmail.com>
2017-07-11 14:01:51 -0400

    minor fixes

---
+++ config/mailerconfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+    key: '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',
+    fromEmail: 'paige-portfolio@noReply',
+    toEmail: 'testmurph316@gmail.com'
+};
