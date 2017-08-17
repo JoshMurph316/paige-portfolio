e17e395bc76fc981bd1d44fefd7486c9e5ca6901
Joshmurph316 <Joshmurph316@gmail.com>
2017-08-17 14:55:43 -0400

    recheck

--- assets/app/contact/contact.component.ts
+++ assets/app/contact/contact.component.ts
@@ -9,10 +9,14 @@ import { EmailService } from "./email/email.service";
   providers: [EmailService]
 })
 export class ContactComponent implements OnInit {
+  moreInfo = 'no';
 
   constructor() { }
 
   ngOnInit() {
+  }
+  onMoreInfo() {
+    this.moreInfo = this.moreInfo === 'yes' ? 'no' : 'yes';
   }
 
 }
